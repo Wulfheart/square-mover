36023b813b5a246df0f8d5fdf7e26b2af2af1668
wulfheart <dev@alexfwulf.de>
2020-12-12 23:58:57 +0000

    Needs some refinements

--- src/SquareMover.php
+++ src/SquareMover.php
@@ -4,5 +4,53 @@ namespace Wulfheart\SquareMover;
 
 class SquareMover
 {
-    // Build your next great package.
+    protected float $start_x;
+    protected float $start_y;
+    protected float $end_x;
+    protected float $end_y;
+
+    protected function __construct(float $start_x, float $start_y) {
+        $this->start_x = $start_x;
+        $this->start_y = $start_y;
+    }
+
+    public static function from($x, $y): self{
+        return new self($x, $y);
+    }
+
+    public function to($x, $y): self {
+        $this->end_x = $x;
+        $this->end_y = $y;
+        return $this;
+    }
+
+    public function squares(): array {
+        $x0 = $this->start_x;
+        $x1 = $this->end_x;
+        $y0 = $this->start_y;
+        $y1 = $this->end_y;
+        $dx = abs($x1 - $x0);
+        $sx = $x0 < $x1 ? 1 : -1;
+        $dy = abs($y1 - $y0);
+        $sy = $y0 < $y1 ? 1 : -1;
+        $err = ($dx > $dy ? $dx : -$dy) / 2;
+        $e2 = 0;
+        $squares = [];
+        for(;;){
+            $squares[] = ['x' => $x0, 'y' => $y0];
+            if($x0 == $x1 && $y0 == $y1) break;
+            $e2 = $err;
+            if($e2 > -$dx) {
+                $err -= $dy;
+                $x0 += $sx;
+            }
+            if($e2 < $dy) {
+                $err += $dx;
+                $y0 += $sy;
+            }
+        }
+        return $squares;
+    }
+
+
 }
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,12 +3,37 @@
 namespace Wulfheart\SquareMover\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Wulfheart\SquareMover\SquareMover;
 
-class ExampleTest extends TestCase
+class Tests extends TestCase
 {
     /** @test */
-    public function true_is_true()
+    public function has_bresenham_paths()
     {
-        $this->assertTrue(true);
+        $bre = [
+            [
+                "start" => [0, 0],
+                "end" => [-3, -3],
+                "squares" => [
+                    [0, 0], [-1, -1], [-2, -2], [-3, -3]
+                ]
+            ],
+            [
+                "start" => [0, 0],
+                "end" => [-3, 2],
+                "squares" => [
+                    [0, 0], [-1, 0], [-1, 1], [-2, 1], [-2, 2], [-3, 2]
+                ]
+            ]
+        ];
+        foreach ($bre as $b) {
+            $squares = SquareMover::from(...$b["start"])->to(...$b["end"])->squares();
+            foreach ($b["squares"] as $s) {
+                $this->assertContains([
+                    "x" => floatval($s[0]),
+                    "y" => floatval($s[1])
+                ], $squares);
+            }
+        }
     }
 }
